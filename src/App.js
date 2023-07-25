93beedef7a65bd6772c49a1fb852fedd7c1dcbcc
Denys Chornokon <denyschornokon3@gmail.com>
2023-07-25 17:56:16 +0300

    about added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderMobile from './components/HeaderMobile';
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <HeaderMobile />
       <Hero />
+      <About />
     </>
   );
 }
