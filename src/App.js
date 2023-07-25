5ddb492de02cbb9dfd87583a6d5d55b2e05953d2
Denys Chornokon <denyschornokon3@gmail.com>
2023-07-25 20:22:15 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Menu from './components/Menu';
 import Contacts from './components/Contacts';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Menu />
       <Contacts />
+      <Footer />
     </>
   );
 }
