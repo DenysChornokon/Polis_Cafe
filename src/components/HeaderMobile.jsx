c2e50d40a4276018d59cff7fb1c355c61d92f736
Denys Chornokon <denyschornokon3@gmail.com>
2023-07-26 14:18:51 +0300

    js to header added

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -1,28 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './HeaderMobile.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
 
 function HeaderMobile() {
-  const [isMenuVisible, setIsMenuVisible] = useState(true);
 
+  //* MENU-EXIT TOGGLE
+
+  const [isMenuVisible, setIsMenuVisible] = useState(true);
+ 
   function toggleMenu() {
     setIsMenuVisible(!isMenuVisible);
   }
 
+  //* BACKGROUND COLOR TOGGLE
+
+  const bgScrollColor = '#ddd4c3cc';    //? bg color
+  const logoDefaultColor = '#fff';
+  const logoScrollColor = '#404040';
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [bg, setBg] = useState("transparent");
+  const [logoColor, setLogoColor] = useState(logoDefaultColor);
+  const [barsColor, setBarsColor] = useState(logoDefaultColor);
+  const [exitColor, setExitColor] = useState(logoDefaultColor);
+
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollPosition > 0) {
+      setBg(bgScrollColor);
+      setBarsColor(logoScrollColor);
+      setExitColor(logoScrollColor);
+      setLogoColor(logoScrollColor);
+    } else {
+      setBg('transparent');
+      setBarsColor(logoDefaultColor);
+      setExitColor(logoDefaultColor);
+      setLogoColor(logoDefaultColor);
+    }
+  }, [scrollPosition]);
+
+
+
   return (
-    <header className='mobile'>
+    <header className='mobile' style={{ background: bg }}>
       <div className="logo-container">
-        <a href="#" className="logo">Polis</a>
+        <a href="#" className="logo" style={{color: logoColor}}>Polis</a>
       </div>
 
       {isMenuVisible ? (
         <div className="menuIconContainer">
-          <FontAwesomeIcon className='menuIcon' icon={faBars} onClick={toggleMenu} />
+          <FontAwesomeIcon className='menuIcon' style={{color: logoColor}} icon={faBars} onClick={toggleMenu} />
         </div>
       ) : (
         <div className="closeIconContainer">
-          <FontAwesomeIcon className='closeIcon' icon={faXmark} onClick={toggleMenu} />
+          <FontAwesomeIcon className='closeIcon' style={{color: logoColor}} icon={faXmark} onClick={toggleMenu} />
         </div>
       )}
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import mainImage from '../assets/main-bg.jpg'
 function Hero() {
   return (
     <section className="hero">
-        <img src={mainImage} alt="hero-image" className="heroImage" />
+        <img src={mainImage} alt="" className="heroImage" />
         <div className="heroText">
             <h1 className="heroName hero">Кулінарний дім «Поліс»</h1>
             <p className="heroSlogan hero">де смак зустрічає спогади...</p>
