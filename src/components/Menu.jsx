33f9ee30771e34786528c511e77ded00b26a61db
Denys Chornokon <denyschornokon3@gmail.com>
2023-07-27 22:28:11 +0300

    some save

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,40 +1,14 @@
 import React from 'react'
 import './Menu.css'
-import ExamplePhoto from '../assets/product-1.avif'
 
 function Menu() {
-  return (
-    <section className="menu" id='menu'>
-        <h2 className="menuHeading">Меню</h2>
+    return (
+        <section className="menu" id='menu'>
+            <h2 className="menuHeading">Меню</h2>
 
-        <div className="productContainer">
-            <div className="box">
-                <img src={ExamplePhoto} alt="Product" />
-                <h3>Name</h3>
-                <div className="content">
-                    <span className='span1'>500uah</span>
-                    <span className='span2'>100gr</span>
-                </div>
-            </div>
-            <div className="box">
-                <img src={ExamplePhoto} alt="Product" />
-                <h3>Name</h3>
-                <div className="content">
-                    <span className='span1'>500uah</span>
-                    <span className='span2'>100gr</span>
-                </div>
-            </div>
-            <div className="box">
-                <img src={ExamplePhoto} alt="Product" />
-                <h3>Name</h3>
-                <div className="content">
-                    <span className='span1'>500uah</span>
-                    <span className='span2'>100gr</span>
-                </div>
-            </div>
-        </div>
-    </section>
-  )
+            
+        </section>
+    )
 }
 
 export default Menu
