5249a70a28c27ba2fc824ee0c0a86565039ec079
Denys Chornokon <denyschornokon3@gmail.com>
2023-07-31 22:46:18 +0300

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,32 +7,33 @@ import './Footer.css'
 function Footer() {
   return (
     <footer>
-      <div className="heading-and-navigation">
-        <h2 className="footer-heading">«Поліс»</h2>
-
-        <ul className="navContainer">
-          <li className="navItem"><a href="#" className="i">Головна</a></li>
-          <li className="navItem"><a href="#about" className="i">Про Нас</a></li>
-          <li className="navItem"><a href="#menu" className="i">Меню</a></li>
-          <li className="navItem"><a href="#contacts" className="i">Контакти</a></li>
-        </ul>
-
-      </div>
-
-      <div className="text-and-copyright">
-
-        <p className='text'>Ми пропонуємо домашні страви – не вдома, без клопоту,
-          тільки з свіжих та якісних продуктів, з посмішкою та позитивною енергетикою.
-        </p>
-        <p className="copyright">&#169; All Right Reserved</p>
-
-      </div>
-
-      <div className="socials">
-        <div className="flex">
-          <FontAwesomeIcon className='icon' icon={faInstagram} />
-          <FontAwesomeIcon className='icon' icon={faFacebook} />
-          <FontAwesomeIcon className='icon' icon={faTelegram} />
+      <div className="footer-container">
+        <div className="heading-and-navigation">
+          <h2 className="footer-heading">«Поліс»</h2>
+          <ul className="navContainer">
+            <li className="navItem"><a href="#" className="i">Головна</a></li>
+            <li className="navItem"><a href="#about" className="i">Про Нас</a></li>
+            <li className="navItem"><a href="#menu" className="i">Меню</a></li>
+            <li className="navItem"><a href="#contacts" className="i">Контакти</a></li>
+          </ul>
+        </div>
+        <div className="text">
+          <p className='text'>Ми пропонуємо домашні страви – не вдома, без клопоту,
+            тільки з свіжих та якісних продуктів, з посмішкою та позитивною енергетикою.
+          </p>
+          <div className="flex desktopp">
+            <FontAwesomeIcon className='icon' icon={faInstagram} />
+            <FontAwesomeIcon className='icon' icon={faFacebook} />
+            <FontAwesomeIcon className='icon' icon={faTelegram} />
+          </div>
+        </div>
+        <div className="socials">
+          <p className="copyright">&#169; All Right Reserved</p>
+          <div className="flex mobilee">
+            <FontAwesomeIcon className='icon' icon={faInstagram} />
+            <FontAwesomeIcon className='icon' icon={faFacebook} />
+            <FontAwesomeIcon className='icon' icon={faTelegram} />
+          </div>
         </div>
       </div>
 
