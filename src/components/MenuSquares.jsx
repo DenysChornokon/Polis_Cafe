0a2a3283ac71814b218fe91eaa573b6d39c15e36
Denys Chornokon <denyschornokon3@gmail.com>
2023-08-08 11:33:56 +0300

    dkdk

--- src/components/MenuSquares.jsx
+++ src/components/MenuSquares.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './MenuSquares.css'
-import TestImg from '../assets/mint.jpg'
+import TestImg from '../assets/DSC_1976.jpg'
 
 function MenuSquares() {
     return (
@@ -21,7 +21,7 @@ function MenuSquares() {
             </div>
             <div className="card three">
                 <div className="card-details">
-                    <p className="text-title">Reservation</p>
+                    <p className="text-title">Передзамовлення страв</p>
                     <p className="text-body">Here are the details of the card</p>
                 </div>
                 <button className="card-button">Перейти</button>
